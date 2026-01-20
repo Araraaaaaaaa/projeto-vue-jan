ed12ca38240e793f30e820c2b74dead58cffb805
Nicole <uwunicole.aquino@gmail.com>
2026-01-20 10:25:05 -0300

    changecommit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,10 +3,10 @@
         <h1><router-link to="/"><img src="../assets/logo.png" alt="Logo em formato png"></router-link></h1>
         <nav>
             <ol>
-                <li><router-link to="/about">Organização</router-link></li>
-                <li>Programação</li>
-                <li>Palestras</li>
-                <li>Contato</li>
+                <li><router-link to="/organizacao">Organização</router-link></li>
+                <li><router-link to="/programacao">Programação</router-link></li>
+                <li><router-link to="/palestras">Palestras</router-link></li>
+                <li><router-link to="/contato">Contato</router-link></li>
             </ol>
         </nav>
     </header>
@@ -15,6 +15,7 @@
     ol{
         display:flex;
         list-style-type: none;
+        justify-content: center;
     }
     a{
         color:black;
